26e6f4853f57e153c2fa2a41288f69dcb0cc1d1f
Arto Soppela <arto.soppela@gofore.com>
2023-12-02 12:57:12 +0200

    index from end change

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -1,14 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Threading;
-
-var firstLastPairs = File.ReadAllText("input/example2.txt")
+﻿var firstLastPairs = File.ReadAllText("input/example2.txt")
     .Split('\n')
     .Select(l => $"{l.ToCharArray().First(c => char.IsDigit(c))}{l.ToCharArray().Reverse().First(c => char.IsDigit(c))}");
-Console.Write($"value sum is {firstLastPairs.Select(v=>int.Parse(v)).Sum()}");
+Console.WriteLine($"value sum is {firstLastPairs.Select(v=>int.Parse(v)).Sum()}");
 
 var phrases = new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 firstLastPairs = File.ReadAllText("input/example2.txt")
     .Split('\n')
-    .Select(l => { var t = l; for (var i = 0; i < phrases.Length; i++) { t = t.Replace(phrases[i], phrases[i][0] +(i + 1).ToString() + phrases[i][phrases[i].Length-1]); } return t; })
+    .Select(l => { var t = l; for (var i = 0; i < phrases.Length; i++) { t = t.Replace(phrases[i], phrases[i][0] +(i + 1).ToString() + phrases[i][^1]); } return t; })
     .Select(l => $"{l.ToCharArray().First(c => char.IsDigit(c))}{l.ToCharArray().Reverse().First(c => char.IsDigit(c))}");
-Console.Write($"value sum is {firstLastPairs.Select(v => int.Parse(v)).Sum()}");
+Console.WriteLine($"value sum is {firstLastPairs.Select(v => int.Parse(v)).Sum()}");
